ba3d28506786d2f69bf3a683eaba33c588bd10cf
hejpecher Janssen <hendrik.janssen@pecher.de>
2023-06-22 09:53:23 +0000

    Übernahme abgestimmte Texte (30)

--- src/lib/help/Help30InKartePositionieren.js
+++ src/lib/help/Help30InKartePositionieren.js
@@ -14,21 +14,12 @@ const Component = () => {
             Um die Auswirkungen durch Starkregen in einem bestimmten Bereich des
             Stadtgebietes zu erkunden, geben Sie den Anfang (mindestens zwei
             Zeichen) einer Adresse im Eingabefeld links unten ein. In der
-            inkrementellen Auswahlliste werden Ihnen passende Treffer angeboten.
-            (Wenn Sie weitere Zeichen eingeben, wird der Inhalt der Auswahlliste
-            angepasst.)
+            Auswahlliste werden Ihnen passende Treffer angeboten.
           </p>
           <p>
             Nach der Auswahl eines Treffers aus der Liste wird die Karte auf die
-            zugehörige Position zentriert. Bei Suchbegriffen mit Punktgeometrie
-            (Adresse, Straße) wird außerdem ein großer Maßstab (Zoomstufe 14)
-            eingestellt und ein Marker <Icon name="map-marker" /> auf der
-            Zielposition platziert.
-          </p>
-          <p>
-            Durch Anklicken des Werkzeugs <Icon name="close" /> links neben dem
-            Eingabefeld können Sie die Suche zurücksetzen (Entfernung von Marker
-            bzw. Abdunklung, Löschen des Textes im Eingabefeld).
+            zugehörige Position zentriert. Durch Anklicken des Werkzeugs <Icon name="close" /> links neben dem
+            Eingabefeld können Sie die Suche zurücksetzen.
           </p>
         </div>
       }
